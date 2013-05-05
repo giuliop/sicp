97c163aedba4722567c26de1acc50a8ecb368e47
giulio <giuliop@gmail.com>
2013-05-05 22:03:17 +0200

    up to exercise 3.23

--- 3.scm
+++ 3.scm
@@ -57,10 +57,12 @@
 
 (define (test-runner name . exps)
   (define (iter exps cont)
+    ;(display "-----------\n")(display cont)(newline)(display exps)(newline)(display "-----------\n")
     (cond ((null? exps) (display cont)
                         (if (= cont 1) (display " test OK") (display " tests OK"))
                         (newline))
-          ((equal? (car exps) (cadr exps)) (iter (cddr exps) (+ cont 1)))
+          ((or (eq? (cadr exps) 'ignore) (equal? (car exps) (cadr exps)))
+           (iter (cddr exps) (+ cont 1)))
           (else (display "FAILED test ") (display (+ 1 cont)) (newline)
                 (display (car exps)) (newline)
                 (display (cadr exps)) (newline))))
@@ -387,3 +389,169 @@
                (cycle?-const-space x1) false
                (cycle?-const-space x2) true
                (cycle?-const-space x5) true))
+
+; 3.21
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+    (error "FRONT called with an empty queue" queue)
+    (car (front-ptr queue))))
+
+(define (print-queue q)
+  (front-ptr q))
+
+(define (test-3-21)
+  (let ((q1 '((a) a))
+        (q2 '((a b) b))
+        (q3 '((b) b))
+        (q4 '(() b)))
+    (test-runner "print-queue"
+                 (print-queue q1) '(a)
+                 (print-queue q2) '(a b)
+                 (print-queue q3) '(b)
+                 (print-queue q4) '())))
+
+; 3.22
+
+(define (make-queue)
+  (let ((front-ptr nil )
+        (rear-ptr nil))
+    ; definitions of internal procedures
+    (define (empty?) (null? front-ptr))
+    (define (front)
+      (if (empty?) (error "FRONT called on empty queue")
+        (car front-ptr)))
+    (define (insert! item)
+      (let ((new-pair (cons item nil)))
+        (cond ((empty?)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair)
+               front-ptr)
+              (else
+                (set-cdr! rear-ptr new-pair)
+                (set! rear-ptr new-pair)
+                front-ptr))))
+    (define (delete!)
+      (cond ((empty?) (error "DELETE called on empty queue"))
+            (else (set! front-ptr (cdr front-ptr))
+                  front-ptr)))
+    (define (dispatch m)
+      (cond ((eq? m 'empty?) (empty?))
+            ((eq? m 'front) (front))
+            ((eq? m 'insert!) insert!)
+            ((eq? m 'delete!) (delete!))
+            (else (error 'Unknown operation for queue - ' m))))
+    dispatch))
+
+(define (front-queue queue) (queue 'front))
+(define (empty-queue? queue) (queue 'empty?))
+(define (insert-queue! queue item) ((queue 'insert!) item))
+(define (delete-queue! queue) (queue 'delete!))
+
+(define (list-copy l)
+  (if (null? l) '()
+    (cons (car l) (list-copy (cdr l)))))
+
+(define (test-3-22)
+  (let ((q (make-queue)))
+    (test-runner "make-queue"
+                 (empty-queue? q) true
+                 (list-copy (insert-queue! q 1)) '(1)
+                 (list-copy (insert-queue! q 2)) '(1 2)
+                 (front-queue q) 1
+                 (insert-queue! q 3) '(1 2 3)
+                 (front-queue q) 1
+                 (empty-queue? q) false
+                 (delete-queue! q) '(2 3)
+                 (front-queue q) 2
+                 (delete-queue! q) '(3)
+                 (delete-queue! q) '()
+                 (empty-queue? q) true)))
+
+; 3.23
+
+(define (make-deque) (cons nil nil))
+(define (front-ptr deque) (car deque))
+(define (rear-ptr deque) (cdr deque))
+(define (empty-deque? deque) (null? (front-ptr deque)))
+(define (set-front! deque item) (set-car! deque item))
+(define (set-rear! deque item) (set-cdr! deque item))
+
+(define (get-item deque end)
+  (if (empty-deque? deque)
+    (error "Trying to retrieve item from empty deque" deque)
+    (caar (end deque))))
+
+(define (insert-deque! deque item end)
+  (let ((new-pair (cons (cons item nil) nil)))
+    (cond ((empty-deque? deque)
+           (set-front! deque new-pair)
+           (set-rear! deque new-pair))
+          ((eq? end 'front)
+           (set-cdr! new-pair (front-ptr deque))
+           (set-cdr! (car (front-ptr deque)) new-pair)
+           (set-front! deque new-pair))
+          (else (set-cdr! (rear-ptr deque) new-pair)
+                (set-cdr! (car new-pair) (rear-ptr deque))
+                (set-rear! deque new-pair)))))
+
+(define (front-delete-deque deque)
+  (cond ((empty-deque? deque) (error "Cannot delete from empty deque" deque))
+        (else (set-front! deque (cdr (front-ptr deque)))
+              (or (empty-deque? deque) (set-cdr! (car (front-ptr deque)) nil)))))
+
+(define (rear-delete-deque deque)
+  (cond ((empty-deque? deque) (error "Cannot delete from empty deque" deque))
+        (else (set-rear! deque (cdar (rear-ptr deque)))
+              (if (null? (rear-ptr deque)) (set-front! deque nil)
+                (set-cdr! (rear-ptr deque) nil)))))
+
+(define (front-insert-deque! deque item) (insert-deque! deque item 'front))
+(define (rear-insert-deque! deque item) (insert-deque! deque item 'rear))
+(define (front-deque deque) (get-item deque front-ptr))
+(define (rear-deque deque) (get-item deque rear-ptr))
+
+(define (print-deque d)
+  (define (iter res _d)
+    (if (or (null? _d) (empty-deque? _d)) res
+      (iter (append res (list (caaar _d))) (cons (cdar _d) (cdr d)))))
+  (iter nil d))
+
+(define (test-3-23)
+  (let ((x (make-deque)))
+    (test-runner "make-deque"
+                 (empty-deque? x) #t
+                 (front-insert-deque! x 3) 'ignore
+                 (front-deque x) 3
+                 (front-insert-deque! x 2) 'ignore
+                 (front-insert-deque! x 1) 'ignore
+                 (front-deque x) 1
+                 (empty-deque? x) #f
+                 (rear-insert-deque! x 4) 'ignore
+                 (front-deque x) 1
+                 (rear-deque x) 4
+                 (rear-insert-deque! x 5) 'ignore
+                 (rear-deque x) 5
+                 (print-deque x) '(1 2 3 4 5)
+                 (front-insert-deque! x 0) 'ignore
+                 (front-deque x) 0
+                 (rear-delete-deque x) 'ignore
+                 (rear-delete-deque x) 'ignore
+                 (rear-deque x) 3
+                 (front-deque x) 0
+                 (front-delete-deque x) 'ignore
+                 (front-delete-deque x) 'ignore
+                 (front-delete-deque x) 'ignore
+                 (rear-deque x) 3
+                 (front-deque x) 3
+                 (front-delete-deque x) 'ignore
+                 (empty-deque? x) #t
+                 (rear-insert-deque! x 1) 'ignore
+                 (empty-deque? x) #f
+                 (rear-delete-deque x) 'ignore
+                 (empty-deque? x) #t
+                 )))
